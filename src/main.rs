afc67776748a1b47d4c7bb0acb44a66a7da77e09
Alin-Petru Roșu <rosualinpetru@gmail.com>
2024-08-20 14:30:15 +0300

    Remove json overhead

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 extern crate paillier;
 extern crate serde_json;
-use clap::{Parser, Subcommand};
-use paillier::*;
-use serde_json::json;
 
-/// Struct representing command-line arguments.
+use std::borrow::*;
+use clap::*;
+use paillier::*;
+use serde_json::*;
+use num_traits::*;
+
+
 #[derive(Parser, Debug)]
 #[command(author = None, version, about = None, long_about = None)]
 #[command(propagate_version = true)]
@@ -15,35 +18,26 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    /// Generates a new keypair and prints them.
-    ///
-    /// Parameters:
-    /// - `bit_length`: The bit length for the generated keys.
+    /// Generates a new keypair and prints them
     Keygen {
         #[arg(short, long)]
         bit_length: usize,
     },
-
-    /// Encrypts a given plaintext with the provided encryption key.
-    ///
-    /// Parameters:
-    /// - `plaintext`: The plaintext to encrypt.
-    /// - `ek`: The encryption key in JSON format.
+    /// Encrypts a given plaintext with the provided encryption key
     Encrypt {
+        /// Plaintext to encrypt
         #[arg(short, long)]
-        plaintext: u64,
+        plaintext: String,
+        /// Encryption key in JSON format
         #[arg(short, long)]
         ek: String,
     },
-
-    /// Decrypts a given ciphertext with the provided decryption key.
-    ///
-    /// Parameters:
-    /// - `ciphertext`: The ciphertext to decrypt.
-    /// - `dk`: The decryption key in JSON format.
+    /// Decrypts a given ciphertext with the provided decryption key
     Decrypt {
+        /// Ciphertext to decrypt
         #[arg(short, long)]
         ciphertext: String,
+        /// Decryption key in JSON format
         #[arg(short, long)]
         dk: String,
     },
@@ -64,59 +58,36 @@ fn main() {
         Commands::Encrypt { plaintext, ek } => {
             let ek: EncryptionKey =
                 serde_json::from_str(ek).expect("Failed to deserialize EncryptionKey");
-            let ciphertext = encrypt(&ek, *plaintext);
-            let response = json!({
-                "ciphertext": serde_json::to_string(&ciphertext).expect("Failed to serialize ciphertext"),
-            });
-            println!("{}", response);
+
+            let big_int_value = BigInt::from_str_radix(plaintext, 10)
+                .expect("Failed to parse the string as BigInt");
+            
+                let ciphertext = encrypt(&ek, RawPlaintext(Cow::Owned(big_int_value)));
+            println!("{}", ciphertext.0.to_string());
         }
         Commands::Decrypt { ciphertext, dk } => {
             let dk: DecryptionKey =
                 serde_json::from_str(dk).expect("Failed to deserialize DecryptionKey");
-            let ciphertext: EncodedCiphertext<u64> =
-                serde_json::from_str(ciphertext).expect("Failed to deserialize ciphertext");
-            let plaintext = decrypt(&dk, ciphertext);
-            let response = json!({
-                "plaintext": plaintext,
-            });
-            println!("{}", response);
+            
+            let big_int_value = BigInt::from_str_radix(ciphertext, 10)
+                .expect("Failed to parse the string as BigInt");
+        
+            let plaintext = decrypt(&dk, RawCiphertext(Cow::Owned(big_int_value)));
+            println!("{}", plaintext.0.to_string());
         }
     }
 }
 
-/// Generates a pair of encryption and decryption keys.
-///
-/// Parameters:
-/// - `bit_length`: The bit length for the generated keys.
-///
-/// Returns:
-/// A tuple containing the encryption key (`EncryptionKey`) and the decryption key (`DecryptionKey`).
 fn generate_keys(bit_length: usize) -> (EncryptionKey, DecryptionKey) {
     let (ek, dk) = Paillier::keypair_with_modulus_size(bit_length).keys();
 
     (ek, dk)
 }
 
-/// Encrypts a plaintext message.
-///
-/// Parameters:
-/// - `ek`: A reference to the encryption key.
-/// - `m`: The plaintext message as a `u64`.
-///
-/// Returns:
-/// The encrypted message as an `EncodedCiphertext<u64>`.
-fn encrypt(ek: &EncryptionKey, m: u64) -> EncodedCiphertext<u64> {
+fn encrypt<'a>(ek: &'a EncryptionKey, m: RawPlaintext<'a>) -> RawCiphertext<'a> {
     Paillier::encrypt(ek, m)
 }
 
-/// Decrypts a ciphertext message.
-///
-/// Parameters:
-/// - `dk`: A reference to the decryption key.
-/// - `c`: The ciphertext message as an `EncodedCiphertext<u64>`.
-///
-/// Returns:
-/// The decrypted message as a `u64`.
-fn decrypt(dk: &DecryptionKey, c: EncodedCiphertext<u64>) -> u64 {
+fn decrypt<'a>(dk: &'a DecryptionKey, c: RawCiphertext<'a>) -> RawPlaintext<'a> {
     Paillier::decrypt(dk, c)
 }
